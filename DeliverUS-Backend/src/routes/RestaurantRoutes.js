67f7bfc9ea5e2699573c22d750f89d7061418d6f
migsorrod1 <migsorrod1@alum.us.es>
2025-03-08 18:02:53 +0100

    Implementado ProductController, funciones en RestaurantController y rutas para restaurante

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,23 @@
 import RestaurantController from '../controllers/RestaurantController.js'
+import ProductController from '../controllers/ProductController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index)
+    .post(
+      RestaurantController.create)
 
+  app.route('/restaurants/:restaurantId')
+    .get(
+      RestaurantController.show)
+    .put(
+      RestaurantController.update)
+    .delete(
+      RestaurantController.destroy)
+
+  app.route('/restaurants/:restaurantId/products')
+    .get(
+      ProductController.indexRestaurant)
 }
 export default loadFileRoutes
